de8b13a07ca0840f537ae6be22343f359aa0d522
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-18 03:51:14 +0800

    Create D-Product.vue

---
+++ src/components/_base/ProductDetails/D-Product.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container>
+    <b-row>
+      <DProductOrder />
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import DProductOrder from './D-ProductOrder'
+export default {
+  components: {
+    DProductOrder
+  }
+}
+</script>
