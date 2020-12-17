4554675497c2bdff77142917ec41594badc0f16b
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-18 03:51:38 +0800

    Create Productdetails.vue

---
+++ src/views/Productdetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="Home">
+    <Navbar :roles="role" :msg="MassageValue" />
+    <hr class="mt-lg-3 d-none d-lg-block" />
+    <DProduct />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '../components/_base/Navbar'
+import Footer from '../components/_base/Footer'
+import DProduct from '../components/_base/ProductDetails/D-Product'
+export default {
+  name: 'ProductDetails',
+  components: {
+    Navbar,
+    Footer,
+    DProduct
+  },
+  data() {
+    return {
+      role: 1,
+      MassageValue: 30,
+      id: 0
+    }
+  },
+  created() {
+    this.id = this.$route.params.id
+  }
+}
+</script>
