6908cc1c1cf3f37d07cfa1c973bc8b290518bd6a
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2020-12-19 23:10:17 +0800

    Create D-Button.vue

---
+++ src/components/_base/ProductDetails/D-Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <b-col sm="12" lg="12">
+    <button
+      type="button"
+      @click="addToCart(DButton, Details)"
+      class="btn__toCartnStaf mb-lg-4 mb-3"
+    >
+      Add to Cart
+    </button>
+    <button type="button" class="btn__toCartnStaf">
+      Ask to Staff {{ this.localStorage }}
+    </button>
+  </b-col>
+</template>
+
+<script>
+export default {
+  props: ['DButton', 'Details'],
+  name: 'DButton',
+  methods: {
+    addToCart(data1, data2) {
+      if (data2.Size) {
+        const SetCart = {
+          id_product: data1.id_product,
+          id_historydetails: 1,
+          qty: data2.qty,
+          total: data2.qty * data1.price_product + data2.SizePrice * data2.qty,
+          size_detail: data2.Size,
+          status_delivery: data2.DeliverySet,
+          status_table: data2.Deliverynow
+        }
+        this.$emit('SendCheckout', SetCart)
+      } else {
+        console.log('Input sIZE DULU')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import '../../../assets/css/DetailsProduct/D-Button.css';
+</style>
